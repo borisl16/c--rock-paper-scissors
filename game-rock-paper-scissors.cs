25305349b46ac12523b20b1213c3e7120ab46407
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-08-14 21:27:53 +0100

    nested while for player

--- game-rock-paper-scissors.cs
+++ game-rock-paper-scissors.cs
@@ -3,7 +3,23 @@
     {
         static void Main(string[] args)
         {
-            Random random = new Random(); 
+            Random random = new Random();
+            bool playAgain = true;
+            String player;
+            String bot;
+
+            while (playAgain)
+            {
+                player = "";
+                computer = "";
+
+                while (player != "ROCK" player != "PAPER" player != "SCISSORS") {
+                    Console.Write("Choose wisely: ROCK, PAPER or SCISSORS: ");
+                    player = Console.ReadLine();
+                    player = player.ToUpper();
+                }
+                Console.Write(player);
+            }
 
             Console.ReadKey();
         }
