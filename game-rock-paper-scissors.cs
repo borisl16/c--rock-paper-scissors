5753bbdf4a7e56bdf1599c12fff78ed316998813
Boris Lazarov <lazarov_borislav@yahoo.com>
2022-08-14 21:50:04 +0100

    switch and cases for player

--- game-rock-paper-scissors.cs
+++ game-rock-paper-scissors.cs
@@ -18,7 +18,37 @@
                     player = Console.ReadLine();
                     player = player.ToUpper();
                 }
-                Console.Write(player);
+
+                switch ( random.Next(1,4))
+                {
+                    case 1:
+                        computer = "ROCK"; break;
+                    case 2:
+                        computer = "PAPER"; break;
+                    case 3:
+                        computer = "SCISSORS"; break;
+                }
+
+                Console.WriteLine("Player: " + player);
+                Console.WriteLine("Computer: " + computer);
+
+                switch (player)
+                {
+                    case "ROCK":
+                        if (computer = "ROCK")
+                        {
+                            Console.WriteLine("It's a draw!");
+                        }
+                        else if (computer == "PAPER")
+                        {
+                            Console.WriteLine("You lose!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win!");
+                        }
+                        break;
+                }
             }
 
             Console.ReadKey();
